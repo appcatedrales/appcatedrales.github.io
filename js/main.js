41130b1480a9dc5491adf6be5a062ff43b0ad936
Jesús Pérez <jesusprubio@gmail.com>
2014-05-11 20:10:09 +0200

    New structure.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Simple jQuery event handler
+$(document).ready(function () {
+    'use strict';
+    
+//    $('#clickMe').click(function (e) {
+//        alert('hello world!');
+//        console.log('ddadadadadad');
+//    });
+    
+});
